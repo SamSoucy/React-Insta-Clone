58491c19acf70c22a3603ee23bd485801b64c545
Samsoucy <newf930@outlook.com>
2019-01-14 16:31:38 -0500

    added components inside App

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import dummyData from "./dummy-data";
+import CommentSection from "./components/CommentSection/CommentSection";
+import PostContainer from "./components/PostContainer/PostContainer";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      dummyData: dummyData,
+    };
+  }
   render() {
     return (
       <div className="App">
+      <SearchBar />
+      {this.state.dummyData.map((post)=>{
+        return<PostContainer post={post}/>
+      })}
         
       </div>
     );
